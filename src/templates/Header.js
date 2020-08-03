9dbdf849d7218a4fdc7893dc4d0ada226b84e8ec
reffcp <cristian.covarrubias@rebits.com.mx>
2020-08-03 11:47:24 -0500

    edicion del icono de inicio

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="#">
                         100tifico
                     </a>
                 </h1>
